eb3c48af553c6c7ececfd3bc1d5aa00450b1c57b
Sachete <gabrielasachete@outlook.com>
2023-10-27 17:23:40 -0300

    Inicio

--- Sistema_Hardware/Sistema_Hardware/Sistema_Hardware.cpp
+++ Sistema_Hardware/Sistema_Hardware/Sistema_Hardware.cpp
@@ -1,20 +1,30 @@
-// Sistema_Hardware.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
 
 #include <iostream>
+#include<locale.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "pt_BR.UTF-8");
+	int option=NULL;
+	cout << "PROGRAMA CALCULOS DE COMPONENTES ELETRÔNICOS PARA DESENVOLVIMENTO DE HARDWARE" << endl;
+	cout << "=============================================================================" << endl;
+	cout << "Calcular componentes para o circuito ci-555 astável [1]" << endl;
+	cout << "Calcular frequência com o tempo [2]" << endl;
+	cout << "Calcular frequência sem o tempo [3]" << endl;
+	cout << "Sair [0]" << endl;
+	cin >> option;
+	switch (option) {
+	case 1:
+		cout << "tecoteco";
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	}
+
+	
+
 }
 
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
